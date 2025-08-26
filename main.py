83013b8af12ca1d8f5e93e23512c8c40385d085c
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2025-08-26 19:58:39 -0300

    feat(main): register auth and order routes in FastAPI app

--- main.py
+++ main.py
@@ -3,3 +3,9 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+from routes.auth_routes import auth_router
+from routes.order_routes import order_router
+
+app.include_router(auth_router)
+app.include_router(order_router)

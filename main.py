45e7f2701ba1fdca18f2385064383b1922841e59
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2025-08-27 17:48:01 -0300

    refactor: ajusta imports e remove código não utilizado

--- main.py
+++ main.py
@@ -3,8 +3,18 @@
 from fastapi import FastAPI
 from routes.auth_routes import auth_router
 from routes.order_routes import order_router
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 app = FastAPI()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 app.include_router(auth_router)
 app.include_router(order_router)

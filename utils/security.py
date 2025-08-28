6d4daf7fbad5b02e8ca81c9d37133dd6619f3f7f
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2025-08-28 13:39:27 -0300

    fix(security): corrige imports e boas práticas no create_access_token

--- utils/security.py
+++ utils/security.py
@@ -1,3 +1,25 @@
+from time import timezone
 from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+from datetime import timedelta
+from jose import JWTError, jwt
+from datetime import datetime
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + (
+        expires_delta or timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
+    )
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+

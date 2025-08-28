8de5b53b85a7e8704e08bc38401701df50e8e34c
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2025-08-27 21:37:39 -0300

    feat(models): adiciona Usuario model separado

---
+++ models/usuario_model.py
@@ -0,0 +1,19 @@
+from database.connection import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    nome = Column("nome", String)
+    email = Column("email", String, nullable=False, unique=True)
+    senha = Column("senha", String)
+    ativo = Column("ativo", Boolean)
+    admin = Column("admin", Boolean, default=False)
+
+    def __init__(self, nome, email, senha, ativo=True, admin=False):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.ativo = ativo
+        self.admin = admin

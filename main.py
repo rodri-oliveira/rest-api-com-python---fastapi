a408c47005ca5145f3b1d45738329a6c96ee56ea
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2025-08-28 20:26:26 -0300

    chore(main): remover bcrypt_context duplicado; usar utils/security como fonte única

--- main.py
+++ main.py
@@ -11,10 +11,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-
 app = FastAPI()
-
-bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 app.include_router(auth_router)
 app.include_router(order_router)

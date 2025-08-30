fceac2beeb6a70b889d86a64e4d14ecfc3641662
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2025-08-29 21:38:14 -0300

    feat(orders): valida preço (> 0) e ajusta exemplo no Swagger

--- schemas/order_schema.py
+++ schemas/order_schema.py
@@ -1,8 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, confloat, Field
+from typing import Annotated
 
 
 class OrderSchema(BaseModel):
+    preco: Annotated[confloat(gt=0), Field(example=0)]
+
+    class Config:
+        from_attributes = True
+
+
+class OrderOutSchema(BaseModel):
+    pedido_id: int
+    status: str
     usuario_id: int
+    preco: float
 
     class Config:
         from_attributes = True
